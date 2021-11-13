17e3672788ba6a904b9f2450c59d8aff9b374866
Max Eddy <maxiscool@gmail.com>
2021-11-13 15:03:14 -0800

    Basic functionality.

--- pages/json-formatter.tsx
+++ pages/json-formatter.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/JsonFormatter.module.css";
 
 const JsonFormatter: NextPage = () => {
+  const [value, setValue] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
   return (
     <div className={styles.container}>
       <Head>
@@ -15,10 +18,42 @@ const JsonFormatter: NextPage = () => {
           <a>&larr; Tools</a>
         </Link>
         <h1>JSON Formatter</h1>
-        <textarea className={styles.input}></textarea>
+        {isInvalid && <div>Invalid JSON</div>}
+        <textarea
+          className={styles.input}
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+            if (isInvalid) {
+              setIsInvalid(false);
+            }
+          }}
+        />
         <div>
-          <button>Pretty</button>
-          <button>Compact</button>
+          <button
+            onClick={() => {
+              try {
+                const json = JSON.parse(value);
+                setValue(JSON.stringify(json, null, 2));
+              } catch (error) {
+                setIsInvalid(true);
+              }
+            }}
+          >
+            Pretty
+          </button>
+          <button
+            onClick={() => {
+              try {
+                const json = JSON.parse(value);
+                setValue(JSON.stringify(json));
+              } catch (error) {
+                setIsInvalid(true);
+              }
+            }}
+          >
+            Compact
+          </button>
         </div>
       </main>
     </div>
