a1632850b10b3c75f58d23e819d01dd806e89ea7
Max Eddy <maxiscool@gmail.com>
2021-11-14 16:45:30 -0800

    gh pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NODE_ENV === "production" ? "/tools" : "";
 module.exports = {
   reactStrictMode: true,
-}
+  basePath,
+  assetPrefix: `${basePath}/`,
+};
